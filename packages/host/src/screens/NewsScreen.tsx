97804dc7155e4708e5c0fcb1cefea9189cc0075a
ThanhVT <vutienthanh0210@gmail.com>
2025-05-02 00:08:14 +0700

    save area

--- packages/host/src/screens/NewsScreen.tsx
+++ packages/host/src/screens/NewsScreen.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { View } from 'react-native';
 import ErrorBoundary from '../components/ErrorBoundary';
 import Placeholder from '../components/Placeholder';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const News = React.lazy(() => import('news/App'));
 
 const NewsScreen = () => {
+  const insets = useSafeAreaInsets();
+  
   return (
     <ErrorBoundary name="NewsScreen">
       <React.Suspense
         fallback={<Placeholder label="News and Articles" icon="newspaper" />}>
-        <News />
+        <SafeAreaProvider>
+          <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1 }}>
+            <News />
+          </View>
+        </SafeAreaProvider>
       </React.Suspense>
     </ErrorBoundary>
   );
