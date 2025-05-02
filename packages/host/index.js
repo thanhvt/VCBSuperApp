f7796d992b30a8748c5250fc92078ec7eb5dd0bd
ThanhVT <vutienthanh0210@gmail.com>
2025-05-02 11:18:02 +0700

    "react-native-linear-gradient": "^2.8.3",

--- packages/host/index.js
+++ packages/host/index.js
@@ -3,6 +3,8 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+// Import LinearGradient to ensure native module is registered
+import 'react-native-linear-gradient';
 
 // Only set storage caching in production
 // @todo: fix this to be reliable in both dev and prod
